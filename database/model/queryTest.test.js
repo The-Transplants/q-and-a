f6b919a28b593136fa825184f7c4164fa0d17490
louis09392003 <louis09392003@gmail.com>
2021-10-01 17:56:33 -0600

    test for first query finished

--- database/model/queryTest.test.js
+++ database/model/queryTest.test.js
@@ -1,35 +1,61 @@
 const questions = require('./questions.js');
+describe('GET /qa/question', () => {
 
-it('should return an array of objects with the correct properties', async ()=>{
-  await questions.queryById(44388)
-  .then( ({rows}) => {
-    let result  = rows[0].jsonb_build_object;
-    console.log(result);
-    [
-      ['question_id', 'number'],
-      ['question_body', 'string'],
-      ['question_date', 'number'],
-      ['asker_name', 'string'],
-      ['helpfulness', 'number'],
-      ['reported', 'boolean']
+  jest.setTimeout(90 * 1000);
+  it('should return an array of objects with the correct properties', async () => {
+    await questions.queryById(44388)
+      .then(({ rows }) => {
+        let result = rows[0];
+        [
+          ['question_id', 'number'],
+          ['question_body', 'string'],
+          ['question_date', 'string'],
+          ['asker_name', 'string'],
+          ['question_helpfulness', 'number'],
+          ['reported', 'boolean'],
+          ['answers', 'object']
 
-    ].forEach( property =>{
-      let propName = property[0];
-      let propType = property[1];
-      expect( result ).toHaveProperty( propName );
-      expect( typeof result[propName] ).toBe( propType );
-    });
+        ].forEach(property => {
+          let propName = property[0];
+          let propType = property[1];
+          expect(result).toHaveProperty(propName);
+          expect(typeof result[propName]).toBe(propType);
+        });
+        /* answers */
+        let resultAns = rows[0].answers;
+        const count = 1;
+        let testAns;
+        for (let key in resultAns) {
+          if (count === 1) {
+            testAns = resultAns[key];
+          }
+        }
+        [
+          ['id', 'number'],
+          ['body', 'string'],
+          ['date', 'number'],
+          ['answerer_name', 'string'],
+          ['helpfulness', 'number'],
+          ['photos', 'object']
+
+        ].forEach(property => {
+          let propName = property[0];
+          let propType = property[1];
+          expect(testAns).toHaveProperty(propName);
+          expect(typeof testAns[propName]).toBe(propType);
+        });
+
+        /* photos */
+        let testPhoto = testAns.photos;
+        if (testPhoto.length) {
+          testPhoto.forEach((photo) => {
+            expect(typeof photo).toBe(string);
+          })
+        }
+
+      })
 
   })
 
-});
+})
 
-// SELECT jsonb_build_object(
-//   'question_id', question_id,
-//   'question_body', question_body,
-//   'question_date', question_date,
-//   'asker_name', asker_name,
-//   'helpfulness', question_helpfulness,
-//   'reported', reported
-
-//    )  FROM questions WHERE product_id = 44388;
--- database/model/questions.js
+++ database/model/questions.js
@@ -26,36 +26,13 @@ module.exports = {
         )
        AS answers FROM answers WHERE answers.question_id = questions.question_id)
       FROM questions WHERE product_id = $1`, [product_id])
-     .then( data => {
-      console.log(data.rows)
-      resolve(data)} )
+     .then( data => resolve(data) )
      .catch( reject );
    });
  }
 
 }
 
-  // `SELECT
-  // question_id,
-  // question_body,
-  // question_date,
-  // asker_name,
-  // question_helpfulness,
-  // reported,
-  // (SELECT jsonb_object_agg(
-  //   answers_id, json_build_object(
-  //     'id', answers_id,
-  //     'body', body ,
-  //     'date', answer_date,
-  //     'answerer_name', answerer_name,
-  //     'helpfulness', helpfulness,
-  //     'photos',
-  //     (SELECT array(
-  //       SELECT photos_url FROM photos WHERE photos.answers_id = answers.answers_id
-  //     ) )
-  //   )
-  //   )
-  //  AS answers FROM answers WHERE answers.question_id = questions.question_id)
-  // FROM questions WHERE product_id = $1`
+
 
 
