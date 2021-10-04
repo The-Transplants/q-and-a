bff502f712dfe2f83e962e1d127d2abf85b0f309
louis09392003 <louis09392003@gmail.com>
2021-10-04 15:24:20 -0600

    modify response send code

--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,6 @@ module.exports = {
   'getQuestions': (req, res, next) => {
     questions.queryById(req.query.product_id)
       .then(result => {
-        console.log(result);
         res.status(200).send(result);
       })
       .catch(err => console.log(err));
@@ -13,17 +12,15 @@ module.exports = {
   'getAnswers': (req, res, next) => {
     answers.queryById(req.params.question_id, req.query.page, req.query.count)
       .then(result => {
-        console.log(result);
         res.status(200).send(result);
       })
       .catch(err => console.log(err));
   },
   'postQuestion': (req, res, next) => {
-    console.log(req.body);
     let { product_id, body, name, email } = req.body;
     questions.postQuestion(product_id, body, name, email)
       .then(data => {
-        res.sendStatus(200);
+        res.sendStatus(201).send('CREATED');
       })
       .catch(err => console.log(err));
   },
@@ -32,7 +29,7 @@ module.exports = {
     let { body, name, email, photos } = req.body;
     answers.postAnswer(question_id, body, name, email, photos)
       .then(data => {
-        res.sendStatus(200);
+        res.sendStatus(201).send('CREATED');
       })
       .catch(err => console.log(err));
   },
@@ -40,34 +37,31 @@ module.exports = {
     let { question_id } = req.params;
     questions.updateHelpful(question_id)
       .then(data => {
-        res.sendStatus(200);
+        res.sendStatus(204).send('NO CONTENT');
       })
       .catch(err => console.log(err));
   },
   'updateAnswerHelpful': (req, res, next) => {
-    console.log(req.params.answer_id);
     let { answer_id } = req.params;
     answers.updateHelpful(answer_id)
       .then(data => {
-        res.sendStatus(200);
+        res.sendStatus(204).send('NO CONTENT');
       })
       .catch(err => console.log(err));
   },
   'updateQuestionReport': (req, res, next) => {
-    console.log(req.params.question_id);
     let { question_id } = req.params;
     questions.updateReport(question_id)
       .then(data => {
-        res.sendStatus(200);
+        res.sendStatus(204).send('NO CONTENT');
       })
       .catch(err => console.log(err));
   },
   'updateAnswerReport': (req, res, next) => {
-    console.log(req.params.answer_id);
     let { answer_id } = req.params;
     answers.updateReport(answer_id)
       .then(data => {
-        res.sendStatus(200);
+        res.sendStatus(204).send('NO CONTENT');
       })
       .catch(err => console.log(err));
   }
