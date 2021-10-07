3c49ef22d141436db3ba092a2fe22482735540db
louis09392003 <louis09392003@gmail.com>
2021-10-07 10:41:48 -0600

    api success called to ec2 db

--- database/model/questions.js
+++ database/model/questions.js
@@ -47,7 +47,7 @@ module.exports = {
             `
            INSERT INTO questions(product_id, question_body, asker_name, asker_email)
            VALUES ($1, $2, $3, $4)`, [product_id, body, name, email])
-        )
+        ).catch(err => console.error(err))
       })
       .then( data => resolve() )
       .catch( err => reject() );
